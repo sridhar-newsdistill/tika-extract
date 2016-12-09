e29878b6c5e65bd48fd8010de6b3edae60c631a5
bac003 <neil.bacon@data61.csiro.au>
2016-12-09 17:34:26 +1100

    ui

--- ui/search.js
+++ ui/search.js
@@ -1,9 +1,20 @@
+/*
+ * ToDo:
+ * how to show main and embedded content
+ * how to show main and embedder meta-data
+ * search meta-data (ui to use drop down list of keys from elesticsearch)
+ */
+
+// To convert a path stored in elasticsearch to a URL for the corresponding doc replace fileBase at the start with docUrlBase
+var fileBase = 'Exemplar Unstructured Deidentified data/';
+var docUrlBase = 'http://localhost/atopp/content';
+
 var searchUrl;
 
 function initSearch() {
   searchUrl = window.location.protocol === 'file:'
     ? 'http://localhost:9200/atopp/atopp/_search'
-    : '/es/atopp/_search'; // using nginx proxy to work around CORS issue
+    : '/search'; // using nginx proxy to work around CORS issue
   $('#main').append(createSearchForm());
 }
 
@@ -11,9 +22,12 @@ function initSearch() {
  * Generic
  */
 
+var isDefined = x => typeof x !== 'undefined';
+
 Array.prototype.flatMap = function(f) {
   return this.map(f).flatten();
 }
+
 Array.prototype.flatten = function() {
   return Array.prototype.concat.apply([], this);
 }
@@ -85,9 +99,7 @@ function genTable(data, cols) {
 
 function getField(x, field) {
   var v = x;
-  for (var i = 0; i < field.length; i++) {
-    v = v[field[i]];
-  }
+  for (var i = 0; i < field.length && isDefined(v); i++) v = v[field[i]];
   return v;
 }
 
@@ -104,18 +116,33 @@ function genRow(x, cols) {
 function Col(label, field, handler, tdClass) {
   this.label = label; // label for header
   this.field = field; // array of field names for data
-  this.handler = typeof handler !== 'undefined' ? handler : defaultColHandler; // a function mapping data item => content of table/tr/td
-  this.tdClass = typeof tdClass !== 'undefined' ? tdClass : field[field.length - 1];
+  this.handler = isDefined(handler) ? handler : defaultColHandler; // a function mapping data item => content of table/tr/td
+  this.tdClass = isDefined(tdClass) ? tdClass : field[field.length - 1];
 }
 
 // functions you can use for the above Col.handler
-function defaultColHandler(v) { return v; }
-function scoreColHandler(v) { return v === "NaN" ? "" : v.toPrecision(4); }
-function contentColHandler(v) { return v.replace(/(?:\n *){3,}/g, '\n\n'); }
+var defaultColHandler = v => v;
+var scoreColHandler = v => v === "NaN" ? "" : v.toPrecision(4);
 
 /**
  * Search
  */
+
+function pathColHandler(v) {
+  var name = v.substring(fileBase.length);
+  return $('<a>').attr({ class: 'doc', href: `${docUrlBase}/${name}`, target: '_blank'}).text(name);
+}
+
+var trim = t => isDefined(t) ? $('<span>').addClass('snippet').append(t.trim().replace(/(?:\n *){3,}/g, '\n\n')) : "";
+var highlightContent = arr => isDefined(arr) ? arr.map(v => trim(v)) : [];
+  
+var contentColHandler = x => isDefined(getField(x, ['highlight', 'content']))
+  ? highlightContent(x.highlight.content)
+  : trim(getField(x, ['_source', 'content']));
+
+var embeddedColHandler = x => isDefined(getField(x, ['highlight', 'embedded.content']))
+  ? highlightContent(x.highlight['embedded.content'])
+  : trim(getField(x, ['_source', 'embedded', 'content']));
 
 function createSearchForm() {
   debug('createSearchForm');
@@ -124,35 +151,77 @@ function createSearchForm() {
   var searchForm = $('<div>').attr('id', 'searchForm').append($('<form>').append(
     $('<label>').attr({for: 'query'}).text('query'),
     q,
-    $('<button>').attr({type: 'button'}).text('Search').click(ev => search(q.val(), searchResult))
+    $('<button>').attr({type: 'button'}).text('Search').click(ev => search(q.val(), searchResult, 2, 0))
   ));
   return [ $('<h1>').text('Search'), searchForm, searchResult ];
 }
 
-var searchResultCols = [
-  new Col('Score', ['_score'], scoreColHandler),
-  new Col('Path', ['_source', 'path']),
-  new Col('Content', ['_source', 'content'], contentColHandler)
-];
-
-function createSearchResults(data) {
-  return [
-    $('<div>').addClass('stats').text(data.hits.hits.length.toLocaleString() + ' of ' + data.hits.total.toLocaleString() + ' hits in ' + (data.took * 0.001).toFixed(3) + ' sec'),
-    genTable(data.hits.hits, searchResultCols)
-  ];
+// Pages: [1] 2 3 ... 27
+// show union of: 1st 3 pages, current and 2 on either side, last page; with ellipsis to indicate gaps
+function pages(size, from, totalHits, searchFrom) {
+  var page = Math.floor(from / size + 1);
+  var last = Math.floor((totalHits + size - 1) / size);
+  
+  var pages = [];
+  var add = p => {
+    if((pages.length == 0 || p > pages[pages.length - 1]) && p >= 1 && p <= last) pages.push(p);
+  }
+  for (var i = 1; i <= 3; ++i) add(i);
+  for (var i = page - 2; i <= page + 2; ++i) add(i);
+  add(last);
+  // debug('pages: ', pages);
+      
+  var txt = t => $('<span>').addClass('page').text(t);
+  var link = t => $('<a>').attr({href: t, class: 'page'}).text(t);
+  var d = $('<span>').addClass('pages').text('Pages:');
+  d.append($.map(pages, (val, idx) => {
+    var e = val == page ? txt(val) : link(val);
+    return idx > 0 && val > pages[idx - 1] + 1 ? [ txt('...'), e ] : e;
+  }));
+  $('a', d).on('click', function(ev) {
+    // debug('pages click: ev', ev);
+    ev.preventDefault();
+    var page = $(ev.target).attr('href');
+    searchFrom((page - 1) * size);
+  });
+  return d;
 }
 
-function search(q, elem) {
-  var qry = { query: { multi_match: {
-    query: q,
-    fields: [ 'content', 'embedded:content' ]
-  } } };
-  debug('search: searchUrl =', searchUrl, 'qry =', qry);
+var searchResultCols = [
+  new Col('Score', ['_score'], scoreColHandler),
+  new Col('Document', ['_source', 'path'], pathColHandler),
+  new Col('Content', [], contentColHandler),
+  new Col('Embedded', [], embeddedColHandler)
+];
+
+function search(q, elem, size, from) {
+  var url = from < 1 ? `${searchUrl}?size=${size}` : `${searchUrl}?size=${size};from=${from}`;
+  var q2 = q.trim();
+  var q3 = q2.length === 0
+    ? { match_all: {} } 
+    : { multi_match: {
+        query: q2,
+        fields: [ 'content', 'embedded.content' ]
+      } };
+  var qry = {
+    query: q3,
+    highlight: { fields: {
+      content: {},
+      'embedded.content': {}
+    } }
+  };
+  debug('search: url =', url, 'qry =', qry);
   elem.empty().append($('<img>').attr({ alt: '', src: 'loading.gif', 'class': 'loading' }));
   doAjax(
-    searchUrl, 
+    url, 
     JSON.stringify(qry),
-    data => elem.empty().append(createSearchResults(data)),
+    data => elem.empty().append(
+      $('<div>').addClass('summary').append(
+        pages(size, from, data.hits.total, frm => search(q, elem, size, frm)),
+        $('<span>').addClass('stats').text(`${data.hits.total.toLocaleString()} hits in ${(data.took * 0.001).toFixed(3)} sec`)
+      ),
+      genTable(data.hits.hits, searchResultCols)
+    ),
     msg => error(elem, msg)
   );
 }
